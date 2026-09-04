a1cf077ea3108ea0d71c433eee476ad77ef8cd92
Sam Privett <sam@privett.dev>
2026-09-04 14:57:06 -0700

    fix: don't hard code a *nix path separator

--- rosidl_runtime_rs/build.rs
+++ rosidl_runtime_rs/build.rs
@@ -22,8 +22,8 @@ fn main() {
     #[cfg(not(feature = "use_ros_shim"))]
     {
         let ament_prefix_path_list = get_env_var_or_abort(AMENT_PREFIX_PATH);
-        for ament_prefix_path in ament_prefix_path_list.split(':') {
-            let library_path = Path::new(ament_prefix_path).join("lib");
+        for ament_prefix_path in env::split_paths(&ament_prefix_path_list) {
+            let library_path = ament_prefix_path.join("lib");
             println!("cargo:rustc-link-search=native={}", library_path.display());
         }
     }
